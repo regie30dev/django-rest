94802d7dd00a5bfd43853dd28bef29c28c1bbebe
regie30developer <regieosano@outlook.com>
2025-07-17 01:43:56 +0800

    latest pushed

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-   path('students/', views.studentsView)
+   path('students/', views.studentsView),
+   path('student/<int:pk>', views.studentDetailView),
 ]
